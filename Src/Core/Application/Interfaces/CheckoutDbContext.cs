51affd0d365472bc823d3dc27af470513602ef35
Jake Ryu <jake.ryu@hotmail.co.uk>
2020-05-29 12:15:11 +0900

    Add CheckoutDbContext interface in Application

---
+++ Src/Core/Application/Interfaces/CheckoutDbContext.cs
@@ -0,0 +1,14 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces
+{
+    public interface ICheckoutDbContext
+    {
+        DbSet<Payment> Payments { get; set; }
+        
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
